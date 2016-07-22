c75157d193a67ea54b4601c9d3e59cafd483cea9
Gess Gallardo <hola@gessgallardo.com>
2016-07-21 20:33:09 -0500

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :role
+  before_validation :set_default_role
+
+  def is_admin?
+    !self.role.nil? and self.role.name == "admin"
+  end
+
+  private
+  
+  def set_default_role
+    self.role ||= Role.find_by_name('registered')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
   
+  devise_for :users
+    
   resources :apps
+  resources :users
   
   match '/store-unavailable' => 'home#store_off', :via => :get, as: :store_off
   root "home#index"
